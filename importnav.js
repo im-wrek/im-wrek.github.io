5ca42cf6e8d998c343b21f8ab00a1086e1eeb1ed
wrek <87952649+im-wrek@users.noreply.github.com>
2024-09-25 16:49:41 -0700

    Add files via upload

---
+++ importnav.js
@@ -0,0 +1,20 @@
+function loadNavbar() {
+    document.body.insertAdjacentHTML('beforebegin', `<style>
+    @import "style.css";
+</style>
+<div class="topnav">
+    <a class="active" href="./index.html">Home</a>
+    <a class="active" href="./index.html#Games">Games</a>
+    <a class="active" href="./index.html#Tools">Tools</a>
+</div>`)
+}
+
+function check() {
+    if (document.body) {
+        loadNavbar()
+    } else {
+        setTimeout(check)
+    }
+}
+
+check() // Faster load times
