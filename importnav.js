8e88cf5aad1102063bc2681c7242cacd72c8f4ab
wrek <87952649+im-wrek@users.noreply.github.com>
2024-09-25 16:57:13 -0700

    Update importnav.js

--- importnav.js
+++ importnav.js
@@ -1,8 +1,5 @@
 function loadNavbar() {
-    document.body.insertAdjacentHTML('beforebegin', `<style>
-    @import "style.css";
-</style>
-<div class="topnav">
+    document.body.insertAdjacentHTML('beforebegin', `<div class="topnav">
     <a class="active" href="./index.html">Home</a>
     <a class="active" href="./index.html#Games">Games</a>
     <a class="active" href="./index.html#Tools">Tools</a>
