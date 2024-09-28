df03fe745c13e5407dbc3049b667365a6aea5341
wrek <basharalmeghezel@gmail.com>
2024-09-28 10:01:56 -0700

    Update emergency.js

--- scripts/emergency.js
+++ scripts/emergency.js
@@ -1,11 +1,13 @@
-document.addEventListener("keyup", (ev) => {
-    let key = ev.key.toLowerCase()
-
-    if (key === "backslash"){
-        window.location.href = "https://classroom.google.com/"; 
-    }
-})
-
 document.onload = function(){
-    document.body.insertAdjacentHTML("beforeend", "<footer>Click Backslash '\' at any time to go to Google Classroom")
+    document.body.insertAdjacentHTML("beforeend", "<footer>Press  Slash '/' at any time to go to Google Classroom")
+
+    document.addEventListener("keyup", (ev) => {
+        let key = ev.key.toLowerCase()
+
+        console.log(key)
+    
+        if (key === "slash"){
+            window.location.href = "https://classroom.google.com/"; 
+        }
+    })
 }
